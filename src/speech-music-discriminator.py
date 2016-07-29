ddd25407798a3fb73c7f3cdc8c6d4294c33f30bb
nicktgr15 <nicktgr15@gmail.com>
2016-07-29 23:05:17 +0300

    ffmpeg pre-processing step introduced

--- src/speech-music-discriminator.py
+++ src/speech-music-discriminator.py
@@ -16,7 +16,14 @@ def main():
     parser.add_argument('--input-file', dest='input_file', required=True)
     args = parser.parse_args()
 
-    cmd = ["yaafe", "-c", FEATURE_PLAN, "-r", "22050", args.input_file]
+    input_dir = os.path.split(args.input_file)[0]
+    temp_file = input_dir + "/temp.wav"
+
+    cmd = ["/usr/bin/ffmpeg", "-i", args.input_file, "-ar", "22050", "-ac", "1", "-acodec", "pcm_s16le",
+           temp_file, "-y"]
+    subprocess.check_call(cmd)
+
+    cmd = ["yaafe", "-c", FEATURE_PLAN, "-r", "22050", temp_file]
 
     subprocess.check_output(cmd)
 
@@ -27,11 +34,11 @@ def main():
 
     FEATURE_GROUPS = [features1, features2, features3, features4]
 
-    peaks, convolution_values, timestamps = feat.get_combined_peaks(args.input_file, FEATURE_GROUPS,
+    peaks, convolution_values, timestamps = feat.get_combined_peaks(temp_file, FEATURE_GROUPS,
                                                                     kernel_type="gaussian")
     detected_segments = kernel.calculate_segment_start_end_times_from_peak_positions(peaks, timestamps)
 
-    timestamps, feature_vectors = feat.read_features(features4, args.input_file, scale=True)
+    timestamps, feature_vectors = feat.read_features(features4, temp_file, scale=True)
 
     with open("/opt/speech-music-discrimination/pickled/model.pickle", 'r') as f:
         trained_model = pickle.load(f)
@@ -46,10 +53,12 @@ def main():
     annotated_segments = feat.filter_noisy_labels(annotated_segments)
     annotated_segments = Util.combine_adjacent_labels_of_the_same_class(annotated_segments)
 
-    Util.write_audacity_labels(annotated_segments, os.path.split(args.input_file)[0] + "/annotated-segments.txt")
+    Util.write_audacity_labels(annotated_segments, input_dir + "/annotated-segments.txt")
 
-    for f in glob.glob(os.path.split(args.input_file)[0] + "/*.csv"):
+    for f in glob.glob(input_dir + "/*.csv"):
         os.remove(f)
+
+    os.remove(temp_file)
 
 
 if __name__ == '__main__':
